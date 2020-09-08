4a2ec664a6c2b5bc2a96c42337f565a6542d0274
sedx876 <swatkins876@gmail.com>
2020-09-08 12:58:18 -0400

    added actions/currentUser.js

---
+++ frontend/src/actions/currentUser.js
@@ -0,0 +1,37 @@
+export const setCurrentUser = user => {
+  return {
+    type: "SET_CURRENT_USER",
+    user
+  }
+}
+
+export const clearCurrentUser = () => {
+  return {
+    type: "CLEAR_CURRENT_USER"
+  }
+}
+
+export const login = (credentials, history) => {
+  return dispatch => {
+    return fetch("http://localhost:3001/api/v1/login", {
+      credentials: "include",
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(credentials)
+    })
+      .then(r => r.json())
+      .then(response => {
+        if (response.error) {
+          alert(response.error)
+        } else {
+          dispatch(setCurrentUser(response.data))
+          dispatch(getMyTrips())
+          dispatch(resetLoginForm())
+          history.push('/')
+        }
+      })
+      .catch(console.log)
+  }
+}
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,9 +1,11 @@
 import { createStore, applyMiddleware, compose, combineReducers } from 'redux'
 import thunk from 'redux-thunk'
 import usersReducer from './reducers/users'
+import currentUser from './reducers/currentUser'
 
 const reducer = combineReducers({
-  users: usersReducer
+  users: usersReducer,
+  currentUser
 })
 
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
