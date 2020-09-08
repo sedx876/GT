d9e7a07d9f10c9d1b715cb973f33c01188b267b5
sedx876 <swatkins876@gmail.com>
2020-09-08 07:00:42 -0400

    fixed routes

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,12 +1,13 @@
 Rails.application.routes.draw do  
-  resources :visits
-  resources :trips
-  resources :comments
-  resources :attractions
+  
   namespace :api do
     namespace :v1 do
       resources :locations
       resources :users
+      resources :visits
+      resources :trips
+      resources :comments
+      resources :attractions
     end
   end
 end
