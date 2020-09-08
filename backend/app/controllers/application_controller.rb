80edd9ae7d189e04c254af5e71f28a7f29f0b083
sedx876 <swatkins876@gmail.com>
2020-09-08 08:50:07 -0400

    create-react-app frontend spinup

--- backend/app/controllers/application_controller.rb
+++ backend/app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::API
-end
+  include ::ActionController::Cookies
+  
+   def current_user
+     #User.find_by(id: session[:user_id])
+     User.first
+   end
+ 
+   def logged_in?
+     !!current_user
+   end
+ 
+ end
