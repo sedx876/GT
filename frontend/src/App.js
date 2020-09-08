b9c06f8cc31c921df4b7b51a026525218af1e69f
sedx876 <swatkins876@gmail.com>
2020-09-08 11:12:39 -0400

    created intial store

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,16 @@
-import React from 'react';
-import './styles/App.css';
+import React, { Component } from 'react'
+import './styles/App.css'
 
-function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+export default class App extends Component {
+
+  
+  render() {
+    return (
+      <div>
+        <h1>Hello from the other side</h1>
+      </div>
+    )
+  }
 }
 
-export default App;
+
