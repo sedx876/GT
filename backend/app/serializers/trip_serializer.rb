a35781aefe996e7a61a8381584961ba90157c32f
sedx876 <swatkins876@gmail.com>
2020-09-10 08:11:51 -0400

    hometown location and trip serializers added

---
+++ backend/app/serializers/trip_serializer.rb
@@ -0,0 +1,14 @@
+class TripSerializer
+  include FastJsonapi::ObjectSerializer
+  attributes :name, :start_date, :end_date
+
+  attribute :locations do |trip|
+    trip.locations.map do |loc|
+      {
+        city: loc.city,
+        state: loc.state,
+        country: loc.country
+      }
+    end
+  end
+end
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles/index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 import { Provider } from 'react-redux'
 import store from './store.js'
 
@@ -17,4 +16,4 @@ ReactDOM.render(
 );
 
 
-serviceWorker.unregister();
+
